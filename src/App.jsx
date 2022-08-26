359d30d5d145a5a08eb1288c12c748541f537e55
bastienapp <bkrettly@simplon.co>
2022-08-26 15:15:39 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const todos = [
+    {
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "et porro tempora",
+      completed: true,
+    },
+    {
+      id: 5,
+      title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      completed: false,
+    },
+    {
+      id: 6,
+      title: "qui ullam ratione quibusdam voluptatem quia omnis",
+      completed: true,
+    },
+  ];
+
+  return (
+    <div className='App'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
